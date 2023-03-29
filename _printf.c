dd68484b057fdd5de46777e0a9016b7b172eae92
Melkamu730 <melkamum27@gmail.com>
2023-03-29 04:44:09 -0700

    Update files

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include "main.h"
-void print_buffer(char buffer[], int *buff_ind);
 
+void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
@@ -12,9 +12,11 @@ int _printf(const char *format, ...)
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
+
 	if (format == NULL)
 		return (-1);
 	va_start(list, format);
+
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
@@ -34,7 +36,7 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
+					flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
